b2558b1252899700861d9df75c025617fbb4c28a
venelrene <venelrene@gmail.com>
2018-03-10 20:18:15 -0500

    user personal timeline is set, so is a footer and navigation
    Next I will be working on the home Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :tweets
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root 'welcome#index'
+
+  get 'tweets/personal', to: 'tweets#personal'
 
   get '/auth/:provider/callback', to: 'sessions#create'
 
   get '/logout', to: 'sessions#destroy'
 
   # resources :users, only: [:index, :show]
-
+  resources :tweets
 end
